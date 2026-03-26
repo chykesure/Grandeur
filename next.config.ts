b8742d61f93eec8285ea6de6492586fd291076bd
Polycarp Chike Pascal <polycarpchike@gmail.com>
2026-03-27 00:10:13 +0100

    Error compiled

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  /* config options here */
+  images: {
+    unoptimized: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
